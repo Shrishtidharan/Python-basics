d42a7c7bd9905d68617fe72559b2d2f14dab846c
Shrishtidharan K A <68120150+Shrishtidharan@users.noreply.github.com>
2022-09-29 15:23:57 +0530

    Update SumofPrime.py

--- SumofPrime.py
+++ SumofPrime.py
@@ -3,14 +3,12 @@ def isPrime(x):
     for a in range(2,x//2 + 1):
         if x % a == 0:
             flag = False
-            return False
             break
-    else:
-        return True
+    return flag
 arr = [2,4,5,7,8]
 sum = 0
 for qq in arr:
     if isPrime(qq):
+        print(qq,end="  ")
         sum += qq
 print("Sum of prime number is - ",sum)
-        
